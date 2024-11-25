11df49b00caa712dd561a6588d3c18918b657e68
Vitor Rosenbergre <vitorosenbergre@gmail.com>
2024-11-24 22:05:50 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const result = JSON.parse(fs.readFileSync('./package.json'));
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+});
